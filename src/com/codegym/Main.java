7c9fab5f6e6d0bc106d2afeb6f59135fbba626ff
bachngocng <nnbach97@gmail.com>
2022-01-07 15:03:53 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,16 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        long sum = 0;
+        stopWatch.start();
+        ;
+        for (int i = 0; i < 100000000; i++) {
+            sum += i;
+        }
+        stopWatch.stop();
+        System.out.println("thời gian chạy của chương trình là: " + stopWatch.getElapsedTime() + "ms");
+    }
+}
